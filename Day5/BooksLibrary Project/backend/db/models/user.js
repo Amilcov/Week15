cf0e12ae62c32b3949f6cec338b9d9e8aea8e35b
Amilcov <Amilcov@yahoo.com>
2022-11-18 17:40:07 +0200

    Add files via upload

---
+++ Day5/BooksLibrary Project/backend/db/models/user.js
@@ -0,0 +1,58 @@
+'use strict';
+const { Validator } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 150]
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 150]
+      },
+    },
+    type: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 1]
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error('Cannot be an email.');
+          }
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 256]
+      },
+    },
+    
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      },
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
