25bdd0374decf0b8f803f546b2dee4081832558a
Amilcov <Amilcov@yahoo.com>
2022-11-23 23:25:00 +0200

    Add files via upload

---
+++ Day5/BooksLibrary Project/v19/backend/db/models/author.js
@@ -0,0 +1,51 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Author extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+       const mappingColumns = {
+        through: "BookAuthor",
+        foreignKey: 'authorId',
+        otherKey: 'bookId',
+      };
+
+      Author.belongsToMany(models.Book, mappingColumns);
+    
+    }
+  }
+  Author.init({
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 150]
+      },
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 150]
+      },
+    },
+    CNP: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true
+    },
+    about: {
+      allowNull: true,
+      type: DataTypes.STRING
+    }
+  }, {
+    sequelize,
+    modelName: 'Author',
+  });
+  return Author;
+};
